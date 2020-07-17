f97160927d3152a51e54e3d4c8e2aac1d450a2ee
Ignacio Aguilera <jiaguilera@gmail.com>
2020-07-17 20:34:16 -0300

    second day

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -34,8 +34,7 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {
-    }
+    fn from(s: &str) -> Person {}
 }
 
 fn main() {
